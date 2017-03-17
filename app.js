13c9782d1caee23d582cb43032212bf0eb21c12e
Nosisky <nosisky@gmail.com>
2017-03-17 15:57:42 +0100

    final commit

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ document.addEventListener("submit", function(event){
     if(strUser === 'meeting'){
          obj = {0: 'None', 1: 'A', 2:'B', 3:'C', 4: 'A and B', 5:'A and C', 6: 'B and C', 7:'A, B and C'};
     }
-    else if(strUser === 'games'){
+    else if(strUser === 'gaming'){
         obj = {0: 'None', 1: 'D', 2:'E', 3:'F', 4: 'D and E', 5:'D and F', 6: 'D and F', 7:'D, E and F'};
     }
-    else if (strUser === 'quite_time'){
+    else if (strUser === 'quiet'){
         obj = {0: 'None', 1: 'G', 2:'H', 3:'I', 4: 'G and H', 5:'G and I', 6: 'H and I', 7:'G, H and I'};
     }
     else{
@@ -21,19 +21,21 @@ document.addEventListener("submit", function(event){
     if(x > 7){
         x = 7;
     }
+    let str = strUser;
      strUser = obj[x];
+
     if(strUser === 'None') {
-       document.getElementById("avail").innerText = "No rooms are available!";
+       document.getElementById("avail").innerText = "No rooms are available in " +str +" rooms";
        // alert("No Rooms are available");
     }
     else{
         if(strUser.length === 1 || strUser.length === 1 || strUser.length === 1){
-        	document.getElementById("avail").innerHTML = 'The available room is ' + "Room " +obj[x];
+        	document.getElementById("avail").innerHTML = 'The available rooms in '+str + " room are " +obj[x];
           // alert('The available room is ' + "Room" +obj[x]);
         }
         else{
 
-        document.getElementById("avail").innerHTML = 'The available rooms are ' + "Room " +obj[x];
+        document.getElementById("avail").innerHTML = 'The available rooms in '+ str + " room are " +obj[x];
            //alert('The available rooms are ' + "Room" +obj[x]);
         }
     }
